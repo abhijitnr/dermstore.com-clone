118d3ba18521032a72ae7279dc29dfbc762d4116
1Gireesh <gireeshbhat68@gmail.com>
2022-11-12 19:28:33 +0530

    addded day-5

---
+++ frontend/dermstore/src/Components/cart_components/GiftCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Box } from "@chakra-ui/react";
+import CartCSS from "../../pages/cart_section/Cart.module.css";
+import { BsChevronUp } from "react-icons/bs";
+function GiftCard() {
+  return (
+    <Box className={CartCSS.giftSection} px='1rem' >
+      <div>
+        <h2>Gift Section</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <span>
+        <BsChevronUp size={22} style={{ fontWeight: "bolder" }} />
+      </span>
+    </Box>
+  )
+}
+
+export default GiftCard
